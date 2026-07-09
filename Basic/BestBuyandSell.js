5bfb69e5f1c4353adc5c6db8786fb37036b01caf
Rishi Rana <rishirana012354@gmail.com>
2026-07-09 19:55:43 +0530

    feat: add implementations for Best Buy and Sell stock algorithm and Reverse Linked List stub

---
+++ Basic/BestBuyandSell.js
@@ -0,0 +1,26 @@
+
+
+const maxProfit = function (prices) {
+
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length - 1; i++) {
+
+        if (prices[i] < minPrice) {
+
+            minPrice = prices[i]
+            console.log("minPrice", minPrice, i)
+        } else {
+            const currentProfit = prices[i] - minPrice;
+
+            maxProfit = Math.max(currentProfit, maxProfit);
+            console.log("currentProfit", currentProfit, i)
+            console.log("maxProfit", maxProfit, i)
+        }
+    }
+    return maxProfit;
+
+}
+
+console.log(maxProfit([7,1,5,3,6,4]));
